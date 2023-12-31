b26a2d31bedab8887090059eb469b95a53dd0486
RemiDFU <remi.dufau08@gmail.com>
2023-12-31 22:02:57 +0100

    update ts + add png

--- src/model/time_serie.py
+++ src/model/time_serie.py
@@ -2,6 +2,7 @@ import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import matplotlib.pyplot as plt
+import numpy as np
 
 def forecast_log_levels(csv_path, column_name, arima_order, forecast_steps):
     """
@@ -34,6 +35,9 @@ def forecast_log_levels(csv_path, column_name, arima_order, forecast_steps):
     # Prévisions
     predictions = model_fit.forecast(steps=forecast_steps)
 
+    # Calcul des métriques d'évaluation
+    y_true = df_filtered[-forecast_steps:]
+
     # Visualisation
     plt.figure(figsize=(10,6))
     plt.plot(df_filtered, label=f'Nombre de logs {column_name} par heure')
@@ -44,5 +48,22 @@ def forecast_log_levels(csv_path, column_name, arima_order, forecast_steps):
     plt.legend()
     plt.show()
 
+    # Aligner les données de prévision avec les horodatages correspondants dans y_true
+    predictions.index = y_true.index
+
+    # Calcul des métriques d'évaluation
+    mse = np.mean((y_true - predictions) ** 2)
+    mae = np.mean(np.abs(y_true - predictions))
+    rmse = np.sqrt(mse)
+    mape = np.mean(np.abs((y_true - predictions) / y_true)) * 100
+
+    # Affichage des métriques
+    print("MSE:", mse)
+    print("MAE:", mae)
+    print("RMSE:", rmse)
+    print("MAPE:", mape)
+
 # RUN func
-forecast_log_levels("../../data/datasets/output_data_2023-12-10_17-06-11.csv", "ERROR", (10, 5, 10), 2)
+forecast_log_levels("../../data/datasets/output_data_2023-12-10_17-06-11.csv", "ERROR", (3, 2, 2), 2)
+
+
